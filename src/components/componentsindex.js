1a34322613ca6b84badd55c788b870c3ce821bd1
Nhoj <nhojplummer@yahoo.com>
2023-10-17 13:39:47 -0400

    Starting more progress

---
+++ src/components/componentsindex.js
@@ -0,0 +1,2 @@
+export {default as Navbar} from "../components/Navbar/Navbar"
+export {default as Creation} from "../components/Creation/Creation"
---
+++ src/containers/indexcontainers.js
@@ -0,0 +1,4 @@
+export {default as HeroSection} from "../containers/HeroSection/HeroSection"
+export {default as Section1} from "../containers/Section1/Section1"
+export {default as Section2} from "../containers/Section2/Section2"
+export {default as Footer} from "../containers/Footer/Footer"
