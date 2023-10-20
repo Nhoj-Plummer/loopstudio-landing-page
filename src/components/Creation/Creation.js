108f4bf86824be83223957867b071e6531decc30
Nhoj <nhojplummer@yahoo.com>
2023-10-20 15:08:11 -0400

    Making something happen hopefully

--- src/components/Creation/Creation.js
+++ src/components/Creation/Creation.js
@@ -1,8 +1,14 @@
 import React from "react"
 import "./Creation.css"
 
-const Creation = () => {
-  return <div>Creation</div>
+const Creation = ({imageStyle}) => {
+  return (
+    <div className="creation-container">
+      <div className="creation-image" style={imageStyle}>
+        <div className="creation-text"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Creation
--- src/containers/Section2/Section2.js
+++ src/containers/Section2/Section2.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./Section2.css"
 import {Creation} from "../../components/componentsindex"
+import {DesktopRoad} from "../../assets/desktop/indexdesktop"
+import {MobileRoad} from "../../assets/mobile/indexmobile"
+
+function getWindowDimensions() {
+  const {innerWidth: width, innerHeight: height} = window
+  return {
+    width,
+    height
+  }
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return windowDimensions
+}
 
 const Section2 = () => {
+  const {width} = useWindowDimensions()
+
+  const graphicStyle1 = {backgroundImage: width > 540 ? {DesktopRoad} : {MobileRoad}}
+
   return (
     <div className="section2-container">
-      <Creation />
+      <Creation imageStyle={graphicStyle1} />
     </div>
   )
 }
