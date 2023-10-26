dc3bb0ddc147b8842352ad75a49726a28f509f7d
Nhoj <nhojplummer@yahoo.com>
2023-10-26 17:28:31 -0400

    Added the hover for the background image

--- src/components/Creation/Creation.js
+++ src/components/Creation/Creation.js
@@ -5,10 +5,8 @@ const Creation = ({imageStyle, text, textStyle}) => {
   return (
     <div className="creation-container">
       <div className="creation-image" style={imageStyle}>
-        <div className="creation-card-overlay">
-          <div className="creation-text" style={textStyle}>
-            <h1>{text}</h1>
-          </div>
+        <div className="creation-text" style={textStyle}>
+          <h1>{text}</h1>
         </div>
       </div>
     </div>
