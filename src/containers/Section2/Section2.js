ad83b782c4e54edea5424307f93cb37918830180
Nhoj <nhojplummer@yahoo.com>
2023-10-26 07:12:07 -0400

    Making progress

--- src/containers/Section2/Section2.js
+++ src/containers/Section2/Section2.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react"
 import "./Section2.css"
 import {Creation} from "../../components/componentsindex"
 import {DesktopEarth} from "../../assets/desktop/indexdesktop"
-import {MobileEarth} from "../../assets/mobile/indexmobile"
+import {MobileEarth, MobileArcade, MobileSoccer, MobileCar, MobileRoad } from "../../assets/mobile/indexmobile"
 
 function getWindowDimensions() {
   const {innerWidth: width, innerHeight: height} = window
@@ -30,22 +30,22 @@ function useWindowDimensions() {
 const Section2 = () => {
   const {width} = useWindowDimensions()
 
-  const image = "https://media.geeksforgeeks.org/wp-content/uploads/rk.png"
-
   const graphicStyle1 = {backgroundImage: width > 540 ? `url(${DesktopEarth})` : `url(${MobileEarth})`}
 
   return (
     <div className="section2-container">
       <div className="section2-container_text">
-        <h1>OUR CREATION</h1>
+        <h1>OUR CREATIONS</h1>
         <div className="section2-container-button desktop">
           <button>
             <h2>SEE ALL</h2>
           </button>
         </div>
       </div>
+      <div className="section2-container-creations">
+        <Creation imageStyle={graphicStyle1} text={"DEEP EARTH"} />
+      </div>
 
-      <Creation imageStyle={graphicStyle1} text={"DEEP EARTH"} />
       <div className="section2-container-button mobile">
         <button>
           <h2>SEE ALL</h2>
