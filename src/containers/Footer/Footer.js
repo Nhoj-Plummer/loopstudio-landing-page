03d75dd528510a5c9c8ec53aa00186ceb20a6e9f
Nhoj <nhojplummer@yahoo.com>
2023-10-27 10:58:38 -0400

    Making progress

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -2,12 +2,42 @@ import React from "react"
 import "./Footer.css"
 import {GlobalLogo, GlobalFacebook, GlobalTwitter, GlobalPinterest, GlobalInstagram} from "../../assets/indexglobal"
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#about">About</a>
+    </p>
+    <p>
+      <a href="#careers">Careers</a>
+    </p>
+    <p>
+      <a href="#events">Events</a>
+    </p>
+    <p>
+      <a href="#products">Products</a>
+    </p>
+    <p>
+      <a href="#support">Support</a>
+    </p>
+  </>
+)
+
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-container_images">
         <img src={GlobalLogo} alt="logo" />
         <div className="footer-container-socials desktop">
+          <img src={GlobalFacebook} alt="" />
+          <img src={GlobalTwitter} alt="" />
+          <img src={GlobalPinterest} alt="" />
+          <img src={GlobalInstagram} alt="" />
+        </div>
+      </div>
+
+      <div className="footer-container-text">
+        <Menu />
+        <div className="footer-container-socials mobile">
           <img src={GlobalFacebook} alt="" />
           <img src={GlobalTwitter} alt="" />
           <img src={GlobalPinterest} alt="" />
